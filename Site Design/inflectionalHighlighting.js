90585b40e4de631cd997d83e4c57e9e4d6fdb39e
Courtney Zelinsky <cmzelinsky@gmail.com>
2014-03-07 09:59:56 -0500

    additional ling schema and ling markup work; + some site work & easter egg ;)

---
+++ Site Design/inflectionalHighlighting.js
@@ -0,0 +1,65 @@
+var allSpans = document.getElementsByTagName("span")
+var colors = { "verb": "green", "inflSuffix": "blue"}
+
+function highlightElement(currentElement)
+{
+    var id = currentElement.getAttribute("id");
+    var color = colors[id];
+    var targets = [];
+    for (var j = 0; j<allSpans.length; j++)
+    {
+        if (allSpans[j].getAttribute("class") == id)
+        {
+            targets.push(allSpans[j]);
+        }
+        for (var i = 0; i < targets.length; i++)
+        {
+            targets[i].style.backgroundColor = color;
+        }
+    }
+}
+
+function highlightElements(id)
+{
+    var color = colors[id];
+    var targets = [];
+    for (var j = 0; j<allSpans.length; j++)
+    {
+        if (allSpans[j].getAttribute("class") == id)
+        {
+            targets.push(allSpans[j]);
+        }
+        for (var i = 0; i < targets.length; i++)
+        {
+            targets[i].style.backgroundColor = color;
+        }
+    }
+}
+    
+function clearHighlights() 
+{   
+    console.log("I've been called!");
+    var target = document.getElementsByTagName('span');
+    for (var i = 0; i < target.length; i++) 
+    {
+        
+        target[i].style.backgroundColor = "";
+     }
+}
+
+function highlightAll()
+{
+    var ids = []
+    for (var j = 0; j<allSpans.length; j++)
+    {
+        var thisId = allSpans[j].getAttribute('id')
+        if (thisId != null && ids.indexOf(thisId) == -1)
+        {
+            ids.push(thisId)
+        }
+    }
+    for (var k = 0; k<ids.length; k++)
+    {
+        highlightElements(ids[k], colors[ids[k]]);
+    }
+}
